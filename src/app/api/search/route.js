8a491f3d9753df085a95c98c18b93125c767b46f
logikmars <dima76673@gmail.com>
2025-12-07 21:09:58 +0200

    fix

--- src/app/api/search/route.js
+++ src/app/api/search/route.js
@@ -1,11 +1,17 @@
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
+  const key = process.env.OPENWEATHER_KEY;
   const { city } = await request.json();
+
+  
+  if (!key) {
+    return res.status(500).json({ error: "Missing API key" });
+  }
 
   try {
     const geoRes = await fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${encodeURIComponent(city)}&limit=1&appid=${process.env.OPENWEATHER_KEY}`
+      `http://api.openweathermap.org/geo/1.0/direct?q=${encodeURIComponent(city)}&limit=1&appid=${key}`
     );
     const geoData = await geoRes.json();
 
@@ -16,7 +22,7 @@ export async function POST(request) {
     const { lat, lon } = geoData[0];
 
     const weatherRes = await fetch(
-      `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely&appid=${process.env.OPENWEATHER_KEY}`
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely&appid=${key}`
     );
     const weatherData = await weatherRes.json();
 
